d09360cc0d38b00b5d5e57acb26579d176e519a4
Ng Poh Ting <sumsnow2@gmail.com>
2025-09-27 17:33:00 +0800

    added unlike and like feature, initial state of 'liked' have a bug

--- src/components/ProfilePostCard.jsx
+++ src/components/ProfilePostCard.jsx
@@ -1,8 +1,11 @@
 import { Button, Col, Image, Row } from 'react-bootstrap';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { jwtDecode } from 'jwt-decode';
 
 export default function ProfilePostCard({ content, postId }) {
-    const [likes, setLikes] = useState(0);
+    let [likes, setLikes] = useState(0);
+    const [liked, setLiked] = useState(false);
 
     useEffect(() => {
         fetch(`https://edd094d0-8e19-471b-bd57-24af5069bce6-00-ytyw0t7ntnoi.pike.replit.dev/likes/post/${postId}`)
@@ -10,6 +13,40 @@ export default function ProfilePostCard({ content, postId }) {
             .then((data) => setLikes(data.length))
             .catch((error) => console.error("Error", error));
     }, [postId]);
+
+    const handleUnlike = () => {
+        likes -= 1;
+        setLiked(false);
+        const token = localStorage.getItem("authToken");
+        axios.delete(
+            `https://edd094d0-8e19-471b-bd57-24af5069bce6-00-ytyw0t7ntnoi.pike.replit.dev/likes/${postId}`,
+            {
+                headers: {
+                    Authorization: token.replace(/^"|"$/g, "") //remove bearers
+                }
+            })
+            .then((response) => console.log('Success', response.data))
+            .catch((error) => console.log('Failed', error));
+    }
+
+    const handleLike = () => {
+        likes += 1;
+        setLiked(true);
+
+        const token = localStorage.getItem("authToken");
+        const decode = jwtDecode(token);
+        const userId = decode.id;
+
+        axios.post(
+            `https://edd094d0-8e19-471b-bd57-24af5069bce6-00-ytyw0t7ntnoi.pike.replit.dev/likes`,
+            {
+                "user_id": userId,
+                "post_id": postId
+            }
+        )
+            .then((response) => console.log('Success', response.data))
+            .catch((error) => console.log('Failed', error));
+    }
 
     return (
         <Row
@@ -35,7 +72,8 @@ export default function ProfilePostCard({ content, postId }) {
                         <i className="bi bi-repeat"></i>
                     </Button>
                     <Button variant="light">
-                        <i className="bi bi-heart">{likes}</i>
+                        {console.log(liked)}
+                        <i className="bi bi-heart" onClick={liked ? handleUnlike : handleLike}>{likes}</i>
                     </Button>
                     <Button variant="light">
                         <i className="bi bi-graph-up"></i>
